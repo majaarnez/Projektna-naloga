d9c627ace4f483eb1598198195b33c86f22a4f4f
majaa <maja.arnez2005@gmail.com>
2026-07-17 17:14:52 +0200

    podatki

--- zajem.py
+++ zajem.py
@@ -1,5 +1,6 @@
 import requests  #knjiznica da pobira iz spletnih strani
 from bs4 import BeautifulSoup
+import pandas as pd
 
 def zajem_julijske_alpe():
     url = "https://www.hribi.net/gorovje/julijske_alpe/1"
@@ -17,16 +18,43 @@ def zajem_julijske_alpe():
             ime = povezava[0]
             visina = povezava[1]
 
+            url_vrha = "https://www.hribi.net" + ime["href"]
+
+            stevilo_poti = zajem_stevilo_poti(url_vrha)
+
             hribi.append({
                 "ime": ime.text.strip(),
-                "visina": visina.text.replace("m", "").strip()
+                "visina": visina.text.replace("m", "").strip(),
+                "stevilo_poti": stevilo_poti
             })
+
+            #print(hribi[-1])
+    
     return hribi
 
-podatki = zajem_julijske_alpe()
+def zajem_stevilo_poti(url):
 
-for hrib in podatki:
-    print(hrib)
+    odgovor = requests.get(url)
+    soup = BeautifulSoup(odgovor.text, "html.parser")
+    besedilo_poti = soup.find(
+        string = lambda besedilo: besedilo and "Število poti:" in besedilo
+    )
+
+    stevilo_poti = None
+
+    if besedilo_poti:
+        povezava_poti = besedilo_poti.parent.find_next("a")
+
+        if povezava_poti:
+            stevilo_poti = povezava_poti.text.strip()
+    return stevilo_poti
+
+
+print("zajem podatkov se je zacel")
+podatki = zajem_julijske_alpe()
+df = pd.DataFrame(podatki)
+df.to_csv("hribi.csv", index=False, encoding="utf-8-sig")
+print("Podatki so shranjeni")
 
     #print(odgovor.status_code) #ce zazenem program mi da 200 to pomeni da koda dela, karkol drucga ne bi delal
     #print(odgovor.text[:500])
